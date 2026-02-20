0a3194c2ff1a3365374e54095cb808fdc8b811ac
mshandywinata <mshandywinata@upi.edu>
2026-02-20 22:02:11 +0700

    feat comment in addTwoPromises

--- addTwoPromises.js
+++ addTwoPromises.js
@@ -1,5 +1,7 @@
 var addTwoPromises = async function(promise1, promise2) {
+    // wait all promise and then return them
     return await Promise.all([promise1, promise2]).then((values) => {
+        // accumulate all result to single value
             return values.reduce((acc, curr) => acc + curr, 0);
         });
 };
