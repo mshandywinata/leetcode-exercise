d1c0d9116c8f5898e739488d8a000b395631d19f
mshandywinata <mshandywinata@upi.edu>
2026-02-21 23:26:28 +0700

    add countPrimeSetBits

---
+++ countPrimeSetBits.js
@@ -0,0 +1,31 @@
+// helper to decide if the value is prime
+var isPrime = function(n) {
+    if (n < 2) return false;
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    } return true;
+}
+
+var countPrimeSetBits = function(left, right) {
+    // initialize how count
+    count = 0;
+
+    // loop over from inclusive left and right value
+    for (let i = left; i <= right; i++) {
+        // convert binary
+        // split to array
+        // count how many '1' in the array
+        let setBits = i.toString(2).split("").filter(x => x === '1').length;
+        console.log(`bit: ${setBits}`);
+
+        // if its prime, increment the count
+        if (isPrime(setBits)) count++;
+    }
+
+    // return count
+    return count;
+}
+
+const result = countPrimeSetBits(6, 10);
+console.log(result);
