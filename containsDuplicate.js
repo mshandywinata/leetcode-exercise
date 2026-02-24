3f581b7f9cdf90ddf767d2073dbf1591f2383d6c
mshandywinata <mshandywinata@upi.edu>
2026-02-25 06:41:29 +0700

    feat comment

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,6 +1,8 @@
 var containsDuplicate = function(nums) {
+    // no need to contain duplicate
     const unique = new Set();
     for (num of nums) {
+        // has means it already contained
         if (unique.has(num)) return true;
         unique.add(num);
     }
