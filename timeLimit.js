cbd612ced82e2b47d135a2af775b3cc063329a6b
mshandywinata <mshandywinata@upi.edu>
2026-02-25 06:39:10 +0700

    feat comment

--- timeLimit.js
+++ timeLimit.js
@@ -1,23 +1,27 @@
+// var timeLimit = function(fn, t) {
+//     return async function(...args) {
+//         const promises = [
+//             new Promise((resolve) => resolve(fn(...args))),
+//             new Promise((resolve, reject) => setTimeout(() => reject("Time Limit Exceeded"), t)),
+//         ];
+
+//         return Promise.race(promises);
+//     }
+// }
+
 var timeLimit = function(fn, t) {
     return async function(...args) {
-        const promises = [
-            new Promise((resolve) => resolve(fn(...args))),
-            new Promise((resolve, reject) => setTimeout(() => reject("Time Limit Exceeded"), t)),
-        ];
-
-        return Promise.race(promises);
-    }
-}
-
-var timeLimit = function(fn, t) {
-    return async function(...args) {
+        // the result would immediately be return
         let res = new Promise((resolve) => {
             resolve(fn(...args))
         });
+        // in case the res not finished up to t
+        // resolve needed because reject always in the second index of Promise args
         let rej = new Promise((resolve, reject) => {
             setTimeout(() => reject("Time Limit Exceeded"), t)
         });
-        
+
+        // only one of those would be matter
         return Promise.race([res, rej]);
     }
 }
